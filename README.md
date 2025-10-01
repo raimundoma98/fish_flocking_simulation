4bef0489a831d21b4a2008dc3c956b116abdbb58
Jose Raimundo <craimundom@gmail.com>
2025-10-01 16:41:33 +0200

    premake5 replaced by cmake

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Fish Flocking Simulation
 Flocking simulation of fishes implemented in C++ using Raylib
+
+# Optimizations implemented
+- Data oriented design (DoD): boid properties are defined as structs of arrays (SoA), instead of arrays of structs (AoS). This improves data locality and CPU cache efficiency.
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
+#include <raylib.h>
+
 #include <iostream>
 
 int main() {
 
     std::cout << "Hello flocking!";
+
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose())
+    {
+      BeginDrawing();
+      ClearBackground(RAYWHITE);
+      DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+      EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 
     return 0;
 }
